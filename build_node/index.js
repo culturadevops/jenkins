c85b926ab1689892cd411c61a121f80b20bba00c
jaivic <jaivicx10a@gmail.com>
2020-02-22 16:14:50 -0500

    node project

---
+++ build_node/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+exports.get = function(event, context, callback) {
+  var contents = fs.readFileSync(`public${path.sep}index.html`);
+  var result = {
+    statusCode: 200,
+    body: contents.toString(),
+    headers: {'content-type': 'text/html'}
+  };
+
+  callback(null, result);
+};
---
+++ build_node/tests/test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var test = require('unit.js');
+var index = require('../index.js');
+
+describe('Tests index', function() {
+  it('verifies successful response', function(done) {
+    index.get({ /* event */ }, { /* context */ }, (err, result) => {
+      try {
+        test.number(result.statusCode).is(200);
+        test.string(result.body).contains('Congratulations');
+        test.value(result).hasHeader('content-type', 'text/html');
+        done();
+      } catch(error) {
+        done(error);
+      }
+    });
+  });
+});
